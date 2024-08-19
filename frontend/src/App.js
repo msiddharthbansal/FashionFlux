2afa356d25b3bef441dd59895a80277aedb5a443
anjSingh453 <166209384+anjSingh453@users.noreply.github.com>
2024-08-19 10:59:30 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { Navbar } from './compoents/Assets/Navbar/Navbar';
+
+//here we setup our routes
+ 
+
+function App() {
+  return (
+    <div  >
+    
+       <Navbar/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/compoents/Assets/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Navbar.css";
+
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+        <ul className="nav-login-cart">
+        <button>Login</button> 
+        </ul>
+
+    </div>
+  )
+}
